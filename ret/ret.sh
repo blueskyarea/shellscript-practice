52c918cb103211ce2dd06d9e5006d3c39dcc1501
mh <mh@dummy.ne.jp>
2014-10-30 22:55:07 +0900

    issue #5 回答の成否により表示メッセージを分岐

--- ret/ret.sh
+++ ret/ret.sh
@@ -2,9 +2,20 @@
 
 SOURCE="012hello345"
 TARGET="hello"
-echo ${SOURCE} から ${TARGET} を抽出する正規表現は？：
+echo -n ${SOURCE} から ${TARGET} を抽出する正規表現は？：
 read answer
+if [[ "${answer}" = "${TARGET}" ]] ; then
+  echo "IT'S NOT REGULAR EXPRESSION."
+  exit
+fi
 if [[ "${SOURCE}" =~ ${answer} ]] ; then
-  echo ${BASH_REMATCH[0]}
+  echo "${BASH_REMATCH[0]}"
+  if [[ "${BASH_REMATCH[0]}" = "${TARGET}" ]] ; then
+    echo "NICE MATCH!!"
+  else
+    echo "BAD MATCH."
+  fi
+else
+  echo "NO MATCH..."
 fi
 
