d1af19b10fba5aca7532e62ac1c58aefd2fb693f
blueskyarea <blueskyareahm@gmail.com>
2022-02-23 09:21:02 +0900

    monitor-process-and-kill for multiple processes.

--- tool/monitor-process-and-kill/mpak.sh
+++ tool/monitor-process-and-kill/mpak.sh
@@ -1,13 +1,16 @@
 #/bin/bash
 
-TARGET_PROCESS=awk
-THRESHOLD_PERCENT=90
+#TARGET_PROCESS=awk
+declare -a TARGET_PROCESSES=("awk" "sed")
+declare -r THRESHOLD_PERCENT=90
 
-total_usage=$(ps aux | grep ${TARGET_PROCESS} | awk 'BEGIN{total=0}{total+=$3}END{print total}')
-echo "Currently, total CPU usage is ${total_usage}% for ${TARGET_PROCESS}"
-if [[ ${total_usage} > ${THRESHOLD_PERCENT} ]];then
+for p in ${TARGET_PROCESSES[@]}; do
+  total_usage=$(ps aux | grep ${p} | awk 'BEGIN{total=0}{total+=$3}END{print total}')
+  echo "Currently, total CPU usage is ${total_usage}% for ${p}"
+  if [[ ${total_usage} > ${THRESHOLD_PERCENT} ]];then
     echo "The usage is over threshold(${THRESHOLD_PERCENT}), killing the process."
-    pkill -n ${TARGET_PROCESS}
-fi
+    pkill -n ${p}
+  fi
+done
 
 #EOF
